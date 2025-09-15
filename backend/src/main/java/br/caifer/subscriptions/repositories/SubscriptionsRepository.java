0b9ae7006564453d798432ce0e8a386a4a4140b0
Caio Barbosa <caiofelipebarbosa92@gmail.com>
2025-09-15 14:11:11 -0300

    fix(repository): annotating with @Repository

--- backend/src/main/java/br/caifer/subscriptions/repositories/SubscriptionsRepository.java
+++ backend/src/main/java/br/caifer/subscriptions/repositories/SubscriptionsRepository.java
@@ -2,8 +2,14 @@ package br.caifer.subscriptions.repositories;
 
 import br.caifer.subscriptions.entities.Subscriptions;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface SubscriptionsRepository extends JpaRepository<Subscriptions, UUID> {
+
+    List<Subscriptions> getAllByUserId(UUID id);
+
 }
